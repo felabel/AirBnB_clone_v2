98933b05c4612143a9d257f3a01f118f2a8dd283
felabel <felicityabel2016@gmail.com>
2024-04-25 06:57:14 +0100

    test-server corrected

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
---creates a database a new user with specified permissions
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
